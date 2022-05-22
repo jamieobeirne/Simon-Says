77be9d67ca290c322fce74ce817ce4b3ff062f8e
Jamie OBeirne <jamieobeirne123@gmail.com>
2022-05-23 00:39:49 +0200

    27 May.2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+
+  firebase: {
+    apiKey: 'AIzaSyDaONtvowrO-DsGTtiYin3888V3DSRRbr4',
+    authDomain: 'simon-says-372d6.firebaseapp.com',
+    projectId: 'simon-says-372d6',
+    storageBucket: 'simon-says-372d6.appspot.com',
+    messagingSenderId: '873554345741',
+    appId: '1:873554345741:web:2699b598a9812fa4e53a44',
+    measurementId: 'G-TZ4NHW3S7X',
+  },
 };
