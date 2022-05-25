89f960ea0b87fbb7916a0e1d2bee838e881eff9a
Jamie OBeirne <jamieobeirne123@gmail.com>
2022-05-25 21:57:12 +0200

    25 May.3

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/app/shared/services/auth.service';
-import { getAuth, updateEmail } from 'firebase/auth';
 import { User } from '@firebase/auth-types';
 
 @Component({
@@ -14,19 +13,15 @@ export class EditProfileComponent implements OnInit {
   constructor(
     private afs: AngularFirestore, 
     public authService: AuthService,
-    _auth = getAuth()
   ) { }
 
   ngOnInit(): void {
   }
 
-  saveChanges(_auth: { user: User; }, uid:string, newName:string, newEmail:string):void{
+  saveChanges(uid:string, newName:string):void{
     let user = this.authService.userInfo;
     this.afs.collection("/users")
     .doc(uid)
-    .update({displayName: newName, email: newEmail})
-    .then(() =>{
-      updateEmail(_auth.user, newEmail!);
-    })  
+    .update({displayName: newName})
   }
 }
